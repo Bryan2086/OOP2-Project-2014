e575ed72e8438854581ee4bde62d810c90d2d698
Bryan2086 <bryan.mccarthy2@students.ittralee.ie>
2014-11-24 14:33:09 +0000

    Removed useless sample files and added first class
    Added the first class of the project which includes the stats of my
    character in my game.

---
+++ Stats.java
@@ -0,0 +1,22 @@
+public class Stats{
+	int strength = 5;
+	int agility = 5;
+	int stamina = 5;
+	int intellect = 5;
+		
+	public int getStr(){
+		return strength;
+	}
+	
+	public int getAgi(){
+		return agility;
+	}
+	
+	public int getStam(){
+		return stamina;
+	}
+	
+	public int getInt(){
+		return intellect;
+	}
+}
